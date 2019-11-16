a9769b889cda8266a7ab52b5ef2010b8ab6661f2
Cyril Garsaud <cyril.garsaud@gmail.com>
2019-11-17 00:43:17 +0100

    This version serves /index.html in case of a 404 error. Useful for nuxt.js, which uses fake urls

--- main.go
+++ main.go
@@ -5,6 +5,35 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("www")))
+	var fs = http.Dir("www");
+	var indexPage = "index.html"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var name = r.URL.Path;
+
+		// Inspect requested path
+		f, err := fs.Open(name)
+		if err != nil {
+			// Fallback to index.html if resource wasn’t found
+			f, _ = fs.Open(indexPage)
+		}
+		defer f.Close()
+		d, _ := f.Stat()
+
+		// Use content of /index.html if requested path is a directory
+		if d.IsDir() {
+			name = name + "/" + indexPage
+			f, err = fs.Open(name)
+			if err != nil {
+				// Fallback to index.html if resource wasn’t found
+				f, _ = fs.Open(indexPage)
+			}
+			defer f.Close()
+			d, _ = f.Stat()
+		}
+
+		http.ServeContent(w, r, d.Name(), d.ModTime(), f)
+	})
+
 	_ = http.ListenAndServe(":80", nil)
 }
